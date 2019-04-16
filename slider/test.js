40315c5b6b2ddd426ec388ed3b7b9217cf8d1977
boileau14 <49596783+boileau14@users.noreply.github.com>
2019-04-16 04:30:50 -0400

    Add files via upload

--- slider/test.js
+++ slider/test.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
     
 function scheduleHover(){
-    $('#nav-schedule')
-        .off('mouseover mouseleave click')
+    $("#nav-schedule")
+        .off("mouseover mouseleave click")
         .on({
             mouseover: function(){
                 TweenMax.to("#nav-schedule div", 0.3, { x: -26, transformOrigin:"50% 50%", ease:Power1.easeInOut });
@@ -11,7 +11,9 @@ function scheduleHover(){
                  TweenMax.to("#nav-schedule div", 0.3, { x: 0, transformOrigin:"50% 50%", ease:Power1.easeInOut });
             },
             click: function(){
-                $('#nav-schedule').off('mouseover mouseleave click');
+                
+                $("#schedule").load( "schedule.html" );
+                $("#nav-schedule").off('mouseover mouseleave click');
                 $("#slider").animate({ left: "+=718px", width: "0", opactity: 0 }, 600);
                 $("#slider-spacer").fadeOut(600);
                     
@@ -19,13 +21,13 @@ function scheduleHover(){
                     
                 setTimeout(function(){ $("#nav-schedule span").removeClass("nav-arrow-tri-down"); },300);
                     /* remove blue? setTimeout(function(){ $("#nav-schedule div").removeClass("nav-arrow-blue"); },300); */
-                $('#nav-schedule div').css({ left: "303px" });
+                $("#nav-schedule div").css({ left: "303px" });
                 
                 TweenMax.to("#nav-schedule div", 0.6, { delay:0.3, x: -26, transformOrigin:"50% 50%", ease:Power1.easeInOut, repeat:0 });
                     
                 setTimeout(function(){ $("#nav-schedule span").addClass("nav-arrow-tri"); },500);
                     /* add red ? setTimeout(function(){ $("#nav-schedule div").addClass("nav-arrow"); },500);*/
-                $('#schedule').css({ left: "0" });
+                $("#schedule").css({ left: "0" });
                 $("#schedule").delay(600).animate({ width: "+=716px",direction: "right" }, 600);
                 $("#content-spacer").delay(600).fadeIn(0);
                 
@@ -33,7 +35,7 @@ function scheduleHover(){
         });
 }
 
-$('#link-1').click(function(){
+$("#link-1").click(function(){
                 TweenMax.to("#nav-schedule div", 0.3, { x: 0, transformOrigin:"50% 50%", ease:Power1.easeInOut });
                 setTimeout(function(){ $("#nav-schedule span").removeClass("nav-arrow-tri"); },300);
                 setTimeout(function(){ $("#nav-schedule span").addClass("nav-arrow-tri-down"); },500);
